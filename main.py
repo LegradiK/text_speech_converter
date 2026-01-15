7845e7d091e40a17fca0d35fe68b11d2cb50241f
Kaho Legradi <legradi.kaho+github@gmail.com>
2026-01-15 16:10:57 +0000

    changes

--- main.py
+++ main.py
@@ -2,26 +2,40 @@ import os
 from dotenv import load_dotenv
 from pathlib import Path
 from pypdf import PdfReader
-from flask import Flask, render_template, request, session, jsonify
+from flask import Flask, render_template, request, session, jsonify, redirect, url_for
+
 
 load_dotenv(dotenv_path=Path(__file__).parent / "data.env")
 
 app = Flask(__name__)
 app.secret_key = os.getenv('FLASK_SECRET_KEY')
 
-speech_file_path = Path(__file__).parent / "speech.mp3"
+def extract_pdf_text(file):
+    reader = PdfReader(file)
+    text = ""
+    for page in reader.pages:
+        text += page.extract_text() + "\n"
 
-reader = PdfReader("Annabel_Lee_Edgar_Allan_Poe.pdf")
-text = ""
-for page in reader.pages:
-    text += page.extract_text() + "\n"
+    extracted_text = text.strip()
+    # print(extracted_text)
 
-extracted_text = text.strip()
-# print(extracted_text)
-
-@app.route("/")
+@app.route("/", methods=["GET"])
 def home():
-    return render_template("home.html", text=extracted_text, speech_file=str(speech_file_path))
+    return render_template("home.html")
+
+@app.route("/upload", methods=["POST"])
+def upload():
+    file = request.files.get("pdf_file")
+
+    if not file or not file.filename.lower().endswith(".pdf"):
+        return redirect(url_for("home"))
+
+    extracted_text = extract_pdf_text(file)
+
+    return render_template(
+        "home.html",
+        extracted_text=extracted_text
+    )
 
 @app.route('/favicon.ico')
 def favicon():
